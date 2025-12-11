2f811849e550af28cdde5f9bc0a3675b216e1140
Avigya Rana <avigyar@gmail.com>
2025-12-11 23:19:44 +0545

    final edit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st 
+from nltk.sentiment import SentimentIntensityAnalyzer
+import nltk
+
+nltk.download("vader-lexicon")
+st.title("sentiment intensity analyzer")
+sia = SentimentIntensityAnalyzer()
+
+text = st.text_area("enter text")
+if st.button("Analyze"):
+    st.write(sia.polarity_scores(text))
